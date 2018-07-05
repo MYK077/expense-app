f292f414438540d65dd51f9ab90cc8bfd0d57b0f
Mayank <myk.vit@gmail.com>
2018-07-05 14:21:52 -0600

    commit

--- expense-manager/client/components/App.js
+++ expense-manager/client/components/App.js
@@ -8,6 +8,7 @@ import Delete from './Delete';
 import { Tab, Tabs } from 'react-bootstrap';
 import YearTabsRouter from './tabs/yearTabsRouter';
 import MonthTabs from './tabs/monthTabs';
+import Chart from './Chart';
 export default class App extends React.Component {
 constructor() {
     super();
@@ -67,7 +68,9 @@ render() {
             }
             </tbody>
 </table>
+
       </div>
+
     );
   }
 }
---
+++ expense-manager/client/components/Chart.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Bar, Line} from 'react-chartjs-2';
+
+class Chart extends Component{
+  constructor(){
+    super(props)
+  }
+  render(){
+    return (
+      <div className ="chart">
+        CHART COMPONENT
+      </div>
+    )
+  }
+}
+
+export default Chart;
